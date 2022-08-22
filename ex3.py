cedb0cf4e89e8e67f4b868eca2f7afe45f9e03aa
mikail rayyan <mikail.r.hernowo@gmail.com>
2022-08-22 08:17:41 +0700

    latihan_2

---
+++ ex3.py
@@ -0,0 +1,24 @@
+print("I will now count my chichen:")
+# line 1 hanya print string pertanyaan
+print("Hens", 25 + 30 / 6)
+print("Roosters", 100 - 25 * 3 % 4)
+# line 3 & 4 mencampur string dan integer di satu print commend
+print("Now I will count the eggs:")
+# line 6 seperti line 1 
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+# line 8 hanya commend yang menyuruh print kalkulasi 
+print("Is it true that 3 + 2 < 5 - 7?")
+# line 10 juga seperti line 1
+print(3 + 2 < 5 - 7)
+# line 12 seperti line 8 hanya commend untuk print kalkulasi 
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+# line 14 & 15 mencampur string dan integer
+print("Oh, that's why it's false.")
+# line 17 hanya print commend
+print("How about some more.")
+# line 19 hanya commend print
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
+# line 21, 22 & 23 mencampur string dan integer yang menghasilkan boolean? 
