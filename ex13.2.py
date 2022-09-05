927f8ffcc407b4e5477fd555f9b46de5cfd197f5
mikail rayyan <mikail.r.hernowo@gmail.com>
2022-09-05 08:22:08 +0700

    latihan_4

---
+++ ex13.2.py
@@ -0,0 +1,15 @@
+import sys
+
+script = str(sys.argv[0])
+first = str(sys.argv[1])
+second = str(sys.argv[2])
+third = str(sys.argv[3])
+
+print(" ")
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your third variable is:", third)
+
+print(" ")
